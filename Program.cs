d02f2b4edc7aa81ddbcb6f96d63f54018c73727f
oleg3790 <oleg.krysko@outlook.com>
2021-05-11 22:28:55 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesforceDataCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
